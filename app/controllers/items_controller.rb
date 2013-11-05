db4d0a5a99d84adcc0a82371406460662f9a4972
Erik <edawebdesign@gmail.com>
2013-11-04 21:53:46 -0500

    Second commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,24 @@
+class ItemsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		@item = Item.new(item_params)
+		
+		@item.save
+		redirect_to @item
+	end
+
+	def show
+		@item = Item.find(params[:id])
+	end
+
+	private
+		def item_params
+			params.require(:item).permit(:name, :notes)
+		end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Wapp::Application.routes.draw do
+  
+  resources :items 
+
+  get "welcome/index"
+  get "static_pages/home"
+  get "static_pages/help"
+  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
