c7909dbb65339a54c89b4d78d518445264ad7a49
Erik <edawebdesign@gmail.com>
2013-11-05 22:41:33 -0500

    title change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Wapp::Application.routes.draw do
   
   resources :items 
- 
+
 
   get "welcome/index"
   get "static_pages/home"
@@ -13,7 +13,7 @@ Wapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'items#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
